5fe1c468520830867173b319fe8322a58e24142b
Ege Önder <ege2003ege@windowslive.com>
2020-08-28 13:27:36 +0300

    :sparkles: heroku test

---
+++ README.md
@@ -0,0 +1,3 @@
+# egeonder.me
+A personal portfolio page built with Node.JS
+You can [click here](https://egeonder.me) to visit the website.
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const path = require('path');
+
+require('dotenv').config();
+
+app = express();
+
+app.use(helmet())
+   .use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+port = process.env.PORT || 1337;
+app.listen(port, () => {
+    console.log(`Listening at PORT ${port}`);
+});
